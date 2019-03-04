38b2861c9113fbcac65935b2079608a5a5280e66
juliojesse25 <juliojesse25@gmail.com>
2019-03-04 13:07:32 -0500

    Added a controller for books

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
   patch '/change-password' => 'users#changepw'
+
+  # Books resources routes
+  get '/books' => 'books#index'
 end
